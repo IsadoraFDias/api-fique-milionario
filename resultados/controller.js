fb8541dc9cbb4a68111518f035e1865911e85ada
Isadora Dias <diasfisadora@gmail.com>
2023-09-22 16:09:26 -0300

    refatorando controller de resultados

--- resultados/controller.js
+++ resultados/controller.js
@@ -3,19 +3,24 @@ import axios from "axios";
 
 const resultado = express.Router();
 
-const apiResultado = 'https://loteriascaixa-api.herokuapp.com/api/{loteria}/{concurso}';
 
 async function getResultado(loteria, concurso) {
-    const response = await axios.get(apiResultado.replace('{loteria}', loteria).replace('{concurso}', concurso));
-    const resultado = response.data;
-    return resultado;
+    const apiResultado = `https://loteriascaixa-api.herokuapp.com/api/${loteria}/${concurso}`;
+    try{
+        const response = await axios.get(apiResultado);
+        const resultado = response.data;
+        return resultado;
+    }catch(error){
+        throw new Error("Erro ao obter o resultado da api");
+    }
 }
 
-resultado.post("/:loteria/:concurso", (req, res) => {
-    const { loteria } = req.body;
+resultado.post("/:loteria/:concurso", async (req, res) => {
+    const loteria  = req.params;
     try {
         if (loteria >= 1 && loteria < 4) {
-            res.status(200).json(numerosSorteados2);
+            const resultado = await getResultado(loteria, concurso);
+            res.status(200).json(resultado);
         } else {
             res.status(400).send("Loteria inválida");
         }
@@ -24,7 +29,13 @@ resultado.post("/:loteria/:concurso", (req, res) => {
     }
 })
 
-resultado.get("/:loteria/:concurso", (req, res) => {
-    const { loteria, concurso } = req.body;
-    res.send(getResultado(loteria, concurso));
-});
+resultado.get("/:loteria/:concurso", async (req, res) => {
+    const { loteria, concurso } = req.params;
+    try {
+        const resultadoData = await getResultado(loteria, concurso);
+        res.status(200).json(resultadoData);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }});
+
+    export default resultado;
